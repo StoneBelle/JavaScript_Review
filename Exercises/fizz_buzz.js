aa69e277a30ac88b87fec82c94d1dd34a112cbcc
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-07-31 12:32:48 -0400

    Update fizz_buzz.js

--- Exercises/fizz_buzz.js
+++ Exercises/fizz_buzz.js
@@ -1,6 +1,8 @@
 var output = [];
+var num = 0; 
 
-function fizzBuzz(num){
+function fizzBuzz(){
+    num ++
     if (num % 3 === 0 && num % 5 === 0){
         output.push("FizzBuzz");
     } else if (num % 3 === 0){
@@ -13,21 +15,21 @@ function fizzBuzz(num){
     return console.log(output)
 }
 
-fizzBuzz(1)
-fizzBuzz(2)
-fizzBuzz(3)
-fizzBuzz(4)
-fizzBuzz(5)
-fizzBuzz(6)
-fizzBuzz(7)
-fizzBuzz(8)
-fizzBuzz(9)
-fizzBuzz(10)
-fizzBuzz(11)
-fizzBuzz(12)
-fizzBuzz(13)
-fizzBuzz(14)
-fizzBuzz(15)
-fizzBuzz(16)
-fizzBuzz(17)
-fizzBuzz(18)
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
