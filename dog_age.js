3b59e6dd320e6ea41b10b4769eb8b7eaf357222f
Stone <65476500+StoneBelle@users.noreply.github.com>
2024-08-04 15:58:07 -0400

    Create dog_age.js

---
+++ dog_age.js
@@ -0,0 +1,6 @@
+// Convert a dog's age into human years
+
+let dogAge = prompt("How old is your dog?: ");
+let humanAge = (dogAge - 2) * 4 + 21;
+
+alert("Your dog is " + humanAge + " years old in human years.")
